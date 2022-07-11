c8a6e279c5cf2c3a7293c78d535ed220c7d37cd0
rabia <rabia.anjum621@gmail.com>
2022-07-11 01:59:10 -0400

    added android native code changes

---
+++ android/app/src/main/java/host/andoid/rabia/ReactOneCustomMethod.java
@@ -0,0 +1,48 @@
+package host.andoid.rabia;
+
+
+import android.annotation.SuppressLint;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Locale;
+
+public class ReactOneCustomMethod extends ReactContextBaseJavaModule {
+    private static ReactApplicationContext reactContext;
+    ReactOneCustomMethod(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "ReactOneCustomMethod";
+    }
+
+    @ReactMethod
+    public void getPhoneID(Promise response) {
+        try {
+            @SuppressLint("HardwareIds") String id = Settings.Secure.getString(reactContext.getContentResolver(), Settings.Secure.ANDROID_ID);
+            response.resolve(id);
+        } catch (Exception e) {
+            response.reject("Error", e);
+        }
+    }
+
+    @ReactMethod
+    public void getDeviceType(Promise response) {
+        try {
+            response.resolve("Android");
+        } catch (Exception e) {
+            response.reject("Error", e);
+        }
+    }
+
+}
